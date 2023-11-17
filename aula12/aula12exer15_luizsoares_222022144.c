7340478bb32d2b6c7d432829855abbe866c85f79
Luiz Henrique Soares <luiz.henrique@confea.org.br>
2023-11-17 17:06:54 -0300

    Atualizando

--- aula12/aula12exer15_luizsoares_222022144.c
+++ aula12/aula12exer15_luizsoares_222022144.c
@@ -4,9 +4,10 @@
 // Entrada :    Valores de ano válidos.
 // Saída :      Média dos anos digitados.
 
-// OBS : Este código foi criado no sistema operacional Ubuntu 22.04 LTS,
-// Logo, alguns comandos podem ser diferentes do Windows Ex: system("cls").
-// Created by luizh-gsoares on 14/11/2023
+// OBS : Este código foi criado no sistema operacional Ubuntu 22.04 LTS.
+// Embora eu tenha um pacote instalado que simula comandos do PowerShell e C
+// pode ser que alguns comandos sejam diferentes e necessita de adaptações.
+// Created by luizh-gsoares on 14/11/2023 - CLion && CodeBlocks
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,45 +16,21 @@
 #define ANO_INICIAL 1900
 #define TAMANHO     3
 
-// FUNÇÃO : fazMedia
-// OBJETIVO : Realizar média de anos de uma matriz 3x3.
-// PARÂMETROS : Matriz 3x3.
-// SAÍDA : Média dos anos digitados.
-float fazMedia(int matriz[TAMANHO][TAMANHO])
-{
-    int soma = 0; int contador = 0;
-
-    for (int i = 0; i < TAMANHO; i++)
-    {
-        for (int j = 0; j < TAMANHO; j++)
-        {
-            if (!(i == 1 && j == 1)) // IGNORA O CENTRO DA MATRIZ
-            {
-                soma += matriz[i][j];
-                contador++;
-            }
-        }
-    }
-
-    return (float)soma / contador;
-};
-
 // FUNÇÃO : validaAno
 // OBJETIVO : Validar se o ano digitado é válido.
 // PARÂMETROS : Ano digitado.
 // SAÍDA : 0 se o ano for válido, 1 se for inválido.
 int validaAno(int ano)
 {
-    if (ano > ANO_INICIAL && ano <= ANO_ATUAL)
+    if (ano >= ANO_INICIAL && ano <= ANO_ATUAL)
     {
         return 0;
     }
     else
     {
-    printf("Ano invalido! Digite novamente: ");
-    return 1;
+        printf("Ano invalido! Digite novamente: ");
+        return 1;
     }
-
 };
 
 // FUNÇÃO : lerAno
@@ -73,6 +50,30 @@ int lerAno()
     return ano;
 };
 
+// FUNÇÃO : fazMedia
+// OBJETIVO : Realizar média de anos de uma matriz 3x3.
+// PARÂMETROS : Matriz 3x3.
+// SAÍDA : Média dos anos digitados.
+float fazMedia(int matriz[TAMANHO][TAMANHO])
+{
+    float soma = 0; int contador = 0;
+
+    for (int i = 0; i < TAMANHO; i++)
+    {
+        for (int j = 0; j < TAMANHO; j++)
+        {
+            if (!(i == 1 && j == 1)) // IGNORA O CENTRO DA MATRIZ
+            {
+                soma += matriz[i][j];
+                contador++;
+            }
+        }
+    }
+
+    matriz[1][1] = soma; // Define o valor da soma na posição [1][1] conforme enunciado.
+    return (float)soma / contador;
+};
+
 // FUNÇÃO : exibeMatriz
 // OBJETIVO : Exibir a matriz 3x3.
 // PARÂMETROS : Matriz 3x3.
@@ -83,7 +84,7 @@ void exibeMatriz(int matriz[TAMANHO][TAMANHO])
     {
         for (int j = 0; j < TAMANHO; j++)
         {
-            printf("%d ", matriz[i][j]);
+            printf("%d\t", matriz[i][j]);
         }
         printf("\n");
     }
@@ -93,23 +94,33 @@ void exibeMatriz(int matriz[TAMANHO][TAMANHO])
 int main()
 {
     int matriz[TAMANHO][TAMANHO] = {0};
+    int continuar = 1;
 
-    for (int i = 0; i < TAMANHO; i++)
-    {
-        for (int j = 0; j < TAMANHO; j++)
-        {
-            if (!(i == 1 && j == 1)) // IGNORA O CENTRO DA MATRIZ
-            {
-            printf("Digite o ano da posicao [%d][%d]: ", i + 1, j + 1);           
-            matriz[i][j] = lerAno();
+    while (continuar) {
+        system("CLS");
+
+        for (int i = 0; i < TAMANHO; i++) {
+            for (int j = 0; j < TAMANHO; j++) {
+                if (!(i == 1 && j == 1)) // IGNORA O CENTRO DA MATRIZ
+                {
+                    printf("Digite o ano da posicao [%d][%d]: ", i + 1, j + 1);
+                    matriz[i][j] = lerAno();
+                    continue;
+                }
+                 printf("POSICAO [%d][%d] RESERVADA PARA A SOMA. \n", i + 1, j + 1);
             }
+            system("CLS");
         }
-        system("cls");
-    }
-    
-    printf("MEDIA:\n");
-    fazMedia(matriz);
 
-    
+        // Exibir dados
+        float media = fazMedia(matriz); // Garantir que exibirMedia seja apresentada antes de exibir a média, conforme saída do enunciado.
+        exibeMatriz(matriz);
+        printf("MEDIA: %.2f \n", media);
+
+        // Continuar
+        printf("Deseja sair? (Digite 0 para sair ou qualquer numero para continuar) : ");
+        scanf("%d", &continuar);
+    };
+
     return 0;
 }
---
+++ trabalhofinal/TF2_LuizSoares_222022144.c
@@ -0,0 +1,279 @@
+// ========== Trabalho Final -  TEMA 2 - CURSOS PROFISSIONALIZANTES  ==========
+// Aluno : Luiz Henrique Guimarães Soares - 222022144
+// Realizado no Sistema Operacional Ubuntu 22.04 LTS
+
+// Objetivo: Gerenciar cursos profissionalizantes oferecidos a jovens carentes no Distrito Federal,
+// permitindo o cadastro, edição, listagem, pesquisa por modalidade e exclusão de cursos,
+// com armazenamento em um arquivo binário.
+
+// Entrada:
+// - Dados para cadastrar um curso: Código de Identificação do Curso (CIC), Nome Completo do Curso,
+// - Sigla da Escola e Modalidade do Curso.
+// - Opções do menu escolhidas pelo usuário.
+
+// Saída:
+// - Relatórios tabelares ordenados alfabeticamente com informações dos cursos.
+// - Mensagens de confirmação, cancelamento ou erro.
+// - Atualização do arquivo binário (cursos.bin) ao encerrar o programa.
+
+// ============================================================================
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXIMO 5
+#define MINIMO 5
+#define MINCIC 10
+#define MAXCURSOS 100
+#define MAXNOME 150
+
+// Struct para armazenar os dados de cada curso
+typedef struct
+{
+    int cic;                        // Código de Identificação do Curso
+    char nomeCompleto[MAXNOME];         // Nome completo do curso
+    char siglaEscola[MAXIMO];       // Sigla da escola
+    char modalidadeCurso;           // Modalidade do curso com char
+} curso;
+
+// ========== PROTÓTIPOS  ==========
+int validaCic(int cic);
+int validaNomeCompleto(char *nomeCompleto);
+int validaSiglaEscola(char *siglaEscola);
+int validaModalidadeCurso(char modalidadeCurso);
+
+// ========== SUBALGORITMOS  ==========
+
+
+// ========== INICIO CADASTRO ==========
+// Função       : lerCic
+// Objetivo     : Ler o CIC do curso
+// Parâmetros   : Nenhum
+// Saída        : CIC do curso
+int lerCic()
+{
+    int cic;    // Código de Identificação do Curso
+    do
+    {
+        fflush(stdin);
+        scanf("%d", &cic);
+    }
+    while (validaCic(cic));
+
+    return cic;
+}
+
+// Função       : validaCic
+// Objetivo     : Validar o CIC do curso
+// Parâmetros   : CIC do curso
+// Saída        : 0 para válido e 1 para inválido
+int validaCic(int cic)
+{
+    return (cic < MINCIC) ? 1 : 0;
+}
+
+
+// Função       : lerNomeCompleto
+// Objetivo     : Ler o nome completo do curso
+// Parâmetros   : Nenhum
+// Saída        : Nome completo do curso
+char* lerNomeCompleto()
+{
+    char nomeCompleto[MAXNOME];    // Nome completo do curso
+
+    do
+    {
+        fflush(stdin);
+        fgets(nomeCompleto, MAXNOME, stdin);
+    }
+    while (validaNomeCompleto(nomeCompleto));
+
+    return nomeCompleto;
+}
+
+// Função       : validaNomeCompleto
+// Objetivo     : Validar o nome completo do curso
+// Parâmetros   : Nome completo do curso
+// Saída        : 0 para válido e 1 para inválido
+int validaNomeCompleto(char *nomeCompleto)
+{
+    return (strlen(nomeCompleto) < MINIMO || strlen(nomeCompleto) > MAXNOME || nomeCompleto[0] == '\n') ? 1 : 0;
+}
+
+// Função       : lerSiglaEscola
+// Objetivo     : Ler a sigla da escola
+// Parâmetros   : Nenhum
+// Saída        : Sigla da escola
+char* lerSiglaEscola()
+{
+    char siglaEscola[MAXIMO];    // Sigla da escola
+    do
+    {
+        fflush(stdin);
+        scanf("%s", siglaEscola);
+    }
+    while (validaSiglaEscola(siglaEscola));
+
+    return siglaEscola;
+}
+
+// Função       : validaSiglaEscola
+// Objetivo     : Validar a sigla da escola
+// Parâmetros   : Sigla da escola
+// Saída        : 0 para válido e 1 para inválido
+int validaSiglaEscola(char *siglaEscola)
+{
+
+    return (strlen(siglaEscola) > MAXIMO || siglaEscola[0] == '\n') ? 1 : 0;
+}
+
+// Função       : lerModalidadeCurso
+// Objetivo     : Ler a modalidade do curso
+// Parâmetros   : Nenhum
+// Saída        : Modalidade do curso
+char lerModalidadeCurso()
+{
+    char modalidadeCurso;    // Modalidade do curso
+
+    do
+    {
+        fflush(stdin);
+        scanf("%c", &modalidadeCurso);
+    }
+    while (validaModalidadeCurso(modalidadeCurso));
+
+    return modalidadeCurso;
+}
+
+// Função       : validaModalidadeCurso
+// Objetivo     : Validar a modalidade do curso
+// Parâmetros   : Modalidade do curso
+// Saída        : 0 para válido e 1 para inválido
+int validaModalidadeCurso(char modalidadeCurso) {
+    return (modalidadeCurso != 'P' && modalidadeCurso != 'T' && modalidadeCurso != 'E') ? 1 : 0;
+}
+
+// Função       : cadastrarCurso
+// Objetivo     : Cadastrar um curso
+// Parâmetros   : Nenhum
+// Saída        : Nenhuma
+curso cadastrarCurso()
+{
+    curso novoCurso;    // Array de cursos
+
+    // ========== CIC ==========
+    printf("Digite o CIC do curso: ");
+    novoCurso.cic = lerCic();
+
+    // ========== NOME COMPLETO ==========
+    printf("Digite o nome completo do curso: ");
+    novoCurso.nomeCompleto[MAXNOME] = lerNomeCompleto();
+
+    // ========== SIGLA DA ESCOLA ==========
+    printf("Digite a sigla da escola: ");
+    novoCurso.siglaEscola[MAXIMO] = lerSiglaEscola();
+
+    // ========== MODALIDADE DO CURSO ==========
+    printf("Digite a modalidade do curso: ");
+    novoCurso.modalidadeCurso = lerModalidadeCurso();
+
+    return novoCurso;
+}
+
+// ========== FIM CADASTRO ==========
+
+
+// ========== MAIN ==========
+int main()
+{
+    curso cursos[MAXCURSOS];    // Array de cursos
+    int opcao;                         // Opção do menu
+
+    do
+    {
+        // ========== MENU ==========
+        printf("1 - Cadastrar curso\n");
+        printf("2 - Editar curso\n");
+        printf("3 - Listar cursos\n");
+        printf("4 - Pesquisar curso por modalidade\n");
+        printf("5 - Excluir curso\n");
+        printf("0 - Sair\n");
+        printf("Digite a opcao desejada: ");
+        scanf("%d", &opcao);
+
+        // ========== SWITCH ==========
+        switch (opcao)
+        {
+        case 1:
+            cursos[0] = cadastrarCurso();
+            break;
+        case 2:
+            break;
+        case 3:
+            printf("%d\n",cursos[0].cic);
+            printf("%s\n",cursos[0].nomeCompleto);
+            printf("%s\n",cursos[0].siglaEscola);
+            printf("%c\n",cursos[0].modalidadeCurso);
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 0:
+            printf("Saindo...\n");
+            break;
+        default:
+            printf("Opcao invalida!\n");
+            break;
+        }
+        fflush(stdin);
+    }
+    while (opcao != 0);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========== SUBPROGRAMAS  ==========
+// Função       :
+// Objetivo     :
+// Parâmetros   :
+// Saída        :
